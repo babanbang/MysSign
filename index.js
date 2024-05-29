65837fa1f966b6a47b44b74e7678890466952686
bbaban <zasd32345@gmail.com>
2024-05-29 19:05:08 +0800

    细节优化

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { Cfg, getDir } from '#Mys.tool'
-Cfg.initCfg('/components/' + getDir(import.meta.url).name, 'sign')
+Cfg.initCfg('/components/', getDir(import.meta.url).name, 'sign')
 
 export * from './Apps/handler.js'
