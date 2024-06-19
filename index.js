077a81c769fa60c3a79c922dd4a681705d397acc
bbaban <zasd32345@gmail.com>
2024-06-19 23:33:22 +0800

    迁移至lib/components

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { Cfg, getDir } from '#MysTool/utils'
-Cfg.initCfg('/components', getDir(import.meta.url).name + '/', 'sign')
+Cfg.initCfg('/lib/components', getDir(import.meta.url).name + '/', 'sign')
 
 export * from './Apps/handler.js'
