25c292df5e71d9b98f09c9d1b0f683207700fedd
bbaban <zasd32345@gmail.com>
2024-07-03 21:40:39 +0800

    控制面板添加配置项

--- defSet/PluginConfigView.js
+++ defSet/PluginConfigView.js
@@ -1,13 +1,13 @@
 import { Data } from "#MysTool/utils"
-const apis = (Data.readdir('model/apis', { Path: import.meta.url }))
-  .filter(p => !['ApiBase.js', 'rrocr_default.js'].includes(p))
-  .map(p => {
+import path from "path"
+const apis = (Data.readdir('model/apis', { Path: path.dirname(import.meta.url) }))
+  .filter(p => !/(ApiBase|_default).js$/.test(p)).map(p => {
     p = p.replace('.js', '')
     return { name: p, value: p }
   })
 
 export default [{
-  name: 'MysSign 配置',
+  name: 'MysSign 基础配置',
   file: 'MysSign-set.yaml',
   priority: 500,
   view: [{
@@ -17,5 +17,20 @@ export default [{
     type: 'select',
     multiple: true,
     item: apis
+  }, {
+    key: '米游社签到任务',
+    comment: '是否开启每日定时米游社签到任务',
+    path: 'signTask',
+    type: 'boolean',
+  }]
+}, {
+  name: 'MysSign 定时任务配置',
+  file: 'MysSign-cron.yaml',
+  priority: 500,
+  view: [{
+    key: '米游社签到任务cron表达式',
+    comment: '默认 0 2 6 * * ? (每天 6 点 2 分执行)',
+    path: 'signTask',
+    type: 'text',
   }]
 }]
